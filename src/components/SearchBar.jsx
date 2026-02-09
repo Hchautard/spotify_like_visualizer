2c6697f341fba2876b32fb2a6260a7f68f781bb4
Hugo <hugo.chautard@etu.univ-amu.fr>
2026-02-09 21:44:48 +0100

    Ajout de la libraire motion

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,13 +1,35 @@
 // SearchBar.jsx
 import { useState } from 'react';
 import { callApi } from '../services/call-api';
+import { animate, stagger, inView } from "motion";
+import LoadingCircleSpinner from '../motions/components/loading-circle-spinner';
+import { s } from 'motion/react-client';
 
 export default function SearchBar() {
   const [query, setQuery] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleSearch = async () => {
-    const res = await callApi(query);
-    console.log(res);
+
+    setIsLoading(true);
+
+    // Simulate API call delay
+    setTimeout(() => {
+      setIsLoading(false);
+      alert(`Search completed for query: "${query}"`);
+    }, 2000);
+
+    // const res = await callApi(query);
+
+    // if (!res) {
+    //   alert('No results found');
+    //   return;
+    // } else if (res.error) {
+    //   alert(`Error: ${res.error}`);
+    //   return;
+    // } else {
+    //   return true 
+    // }
   };
 
   return (
@@ -21,6 +43,7 @@ export default function SearchBar() {
         <button onClick={handleSearch} className="button">
           Search
         </button>
+        {isLoading && <LoadingCircleSpinner />}
       </div>
   );
 }
